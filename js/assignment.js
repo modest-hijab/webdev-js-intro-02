300b59d052c171549e2ccf9c3463b6089b1995aa
modest-hijab <ayah1328@aol.com>
2024-12-18 21:30:08 -0500

    completed assignment webdev js intro 02

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,34 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "read more";
+const currentYear = 2025;
+
+let willMeetResolution = true;
 
 function updateYear() {
     // Update this function
-
+    yearElement.innerText=currentYear;
 }
 
 function updateResolution() {
     // Update this function 
-
+    resolutionElement.innerText=resolution;
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+    willMeetResolutionElement.innerText=willMeetResolution;
 }
 
 function render() {
     // Update this function
-
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+    render()
 })
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*const myName = "Dan";
+console.log(myName);*/
 
